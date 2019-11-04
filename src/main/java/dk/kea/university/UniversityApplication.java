4ee6c5c65af75d5b84970c8dd1f8a2fb96871aa8
Marcus Miyalys <resolutions@riseup.net>
2019-11-04 14:09:16 +0100

    First commit

---
+++ src/main/java/dk/kea/university/UniversityApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/kea/university/UniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
