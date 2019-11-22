53903228cf3436353203f29918be7f1d5a59487e
Marcus Miyalys <resolutions@riseup.net>
2019-11-22 19:07:56 +0100

    H2 console working, as access is no longer blocked by Spring Security

--- code/src/main/java/dk/kea/university/SecurityConfig.java
+++ code/src/main/java/dk/kea/university/SecurityConfig.java
@@ -1,4 +1,4 @@
-package dk.kea.university.bootstrap;
+package dk.kea.university;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+// H2 console
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 // This file can be anywhere in the project. The class name can be whatever, as long as it extends WebSecurityConfigurerAdapter.
 
 @Configuration
@@ -17,21 +20,31 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // These roles are the ones that must match the ones specified with @Secured in the Controllers. The Enum is irrelevant?
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-      auth.inMemoryAuthentication()
-        .withUser("max").password(passwordEncoder().encode("p")).roles("STUDENT")
-        .and()
-        .withUser("eve").password(passwordEncoder().encode("p")).roles("TEACHER")
-        .and()
-        .withUser("sskraek").password(passwordEncoder().encode("p")).roles("TEACHER")
-        .and()
-        .withUser("hacker").password(passwordEncoder().encode("p")).roles("ADMIN");
-    }
+  // These roles are the ones that must match the ones specified with @Secured in the Controllers. The Enum is irrelevant?
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+      .withUser("max").password(passwordEncoder().encode("p")).roles("STUDENT")
+      .and()
+      .withUser("eve").password(passwordEncoder().encode("p")).roles("TEACHER")
+      .and()
+      .withUser("sskraek").password(passwordEncoder().encode("p")).roles("TEACHER")
+      .and()
+      .withUser("hacker").password(passwordEncoder().encode("p")).roles("ADMIN");
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-      return new BCryptPasswordEncoder();
-    }
+  // Spring Security blocks access to the console so therefore we have this section
+  // https://springframework.guru/using-the-h2-database-console-in-spring-boot-with-spring-security/
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers("/").permitAll().and()
+      .authorizeRequests().antMatchers("/console/**").permitAll();
+    http.csrf().disable();
+    http.headers().frameOptions().disable();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
